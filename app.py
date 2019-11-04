22b1d0f206a1c1709e0562f0e9cce6114521e433
Josh Choo <dev@joshuous.com>
2019-11-05 07:49:49 +0800

    feat: add sqlalchemy config

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+from .db import db
 from flask import Flask
 from flask_restful import Api
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 api = Api(app)
+
+db.init_app(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
