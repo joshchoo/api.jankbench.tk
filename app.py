ac05327954aaf4c44482265db6dec8d6323af2e3
Josh Choo <dev@joshuous.com>
2019-11-05 19:41:56 +0800

    refactor: create devices and results database schemas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from .db import db
+from db import db
 from flask import Flask
 from flask_restful import Api
 
---
+++ models/device.py
@@ -0,0 +1,19 @@
+from db import db
+
+
+class DeviceModel(db.Model):
+    __tablename__ = "devices"
+
+    id = db.Column(db.Integer, primary_key=True)
+    benchmark_version = db.Column(db.String(10), nullable=False)
+    device_model = db.Column(db.String(80), nullable=False)
+    device_product = db.Column(db.String(80), nullable=False)
+    device_board = db.Column(db.String(80), nullable=False)
+    device_manufacturer = db.Column(db.String(80), nullable=False)
+    device_brand = db.Column(db.String(80), nullable=False)
+    device_hardware = db.Column(db.String(80), nullable=False)
+    android_version = db.Column(db.String(80), nullable=False)
+    build_type = db.Column(db.String(80), nullable=False)
+    build_time = db.Column(db.String(80), nullable=False)
+    fingerprint = db.Column(db.String(120), nullable=False)
+    kernel_version = db.Column(db.String(200))
