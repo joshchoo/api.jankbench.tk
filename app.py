c4c7562afb0df437023f329b33123ebc2eebb9e3
Josh Choo <dev@joshuous.com>
2019-11-05 19:40:39 +0800

    refactor: create handler for Marshmallow ValidationError

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from db import db
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from ma import ma
+from marshmallow import ValidationError
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -11,6 +12,12 @@ api = Api(app)
 
 db.init_app(app)
 ma.init_app(app)
+
+
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):
+    return jsonify(err.messages), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
