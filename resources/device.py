4b419797f24fd3175fe24a9ba325856bfde3e6b6
Josh Choo <dev@joshuous.com>
2019-11-05 23:33:11 +0800

    feat: implement /devices endpoint

--- resources/device.py
+++ resources/device.py
@@ -1,7 +1,11 @@
 from flask_restful import Resource
 from models.device import DeviceModel
+from schemas.device import DeviceSchema
 
 
 class Devices(Resource):
-    def get(self):
-        pass
+    @classmethod
+    def get(cls):
+        device_list_schema = DeviceSchema(many=True, exclude=("results",))
+        devices = device_list_schema.dump(DeviceModel.query.all())
+        return {"devices": devices}, 200
