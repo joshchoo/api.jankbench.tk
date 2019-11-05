8aae86b4a9a82ade16b5a6fd6932177ef5bbf044
Josh Choo <dev@joshuous.com>
2019-11-05 20:57:33 +0800

    refactor: automatically create tables if necessary

--- web/app.py
+++ web/app.py
@@ -19,6 +19,11 @@ db.init_app(app)
 ma.init_app(app)
 
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 @app.errorhandler(ValidationError)
 def handle_marshmallow_validation(err):
     return jsonify(err.messages), 400
