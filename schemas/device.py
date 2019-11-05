5bdf155c01e337547b3689888a9b46a4aefafe22
Josh Choo <dev@joshuous.com>
2019-11-05 23:32:07 +0800

    refactor: validate test name and include nested fields in Marshmallow schemas

--- schemas/device.py
+++ schemas/device.py
@@ -1,8 +1,11 @@
 from ma import ma
 from models.device import DeviceModel
+from models.result import ResultModel  # DO NOT REMOVE
 
 
 class DeviceSchema(ma.ModelSchema):
+    results = ma.Nested('ResultSchema', many=True, exclude=('device',))
+
     class Meta:
         model = DeviceModel
         dump_only = ("id",)
