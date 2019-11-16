2b89d49463eb256cf41c5eab3c62225d29069b61
Josh Choo <dev@joshuous.com>
2019-11-16 09:44:43 +0800

    refactor: specify additional Flask config on app creation

--- config.py
+++ config.py
@@ -3,9 +3,20 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
+class DefaultConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL", "sqlite:///" + os.path.join(basedir, "data.db")
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
+        "SQLALCHEMY_TRACK_MODIFICATIONS", False
+    )
+
+
+class TestingConfig:
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "sqlite:///" + os.path.join(basedir, "testing.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get(
         "SQLALCHEMY_TRACK_MODIFICATIONS", False
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    # Create tables
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
